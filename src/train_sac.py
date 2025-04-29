06175ad075623b621a0917b30dc4bc087b1ab06a
Stephen McIlvanna <smcilvanna01@qub.ac.uk>
2025-04-29 12:42:06 +0100

    reduced train freq and gradient steps from 10 to 1

--- src/train_sac.py
+++ src/train_sac.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
         batch_size=256,
         tau=0.005,
         gamma=0.99,
-        train_freq=10,
-        gradient_steps=10,
+        train_freq=1,
+        gradient_steps=1,
         policy_kwargs=dict(net_arch=[256, 256])
     )
 
