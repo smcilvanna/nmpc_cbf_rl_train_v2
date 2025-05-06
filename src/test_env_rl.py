493ede168eeae0467731acbd9282413f6774a71f
Stephen McIlvanna <smcilvanna01@qub.ac.uk>
2025-05-06 09:39:17 +0100

    test rl environment script working

--- src/test_env_rl.py
+++ src/test_env_rl.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
     
     # Create wrapped environment
     env = ActionPersistenceWrapper(MPCHorizonEnv(curriculum_level=2), persist_steps=PERSIST_STEPS)
-    with open('./env-1-1.pkl', 'rb') as f: 
+    with open('./env-2-2.pkl', 'rb') as f: 
         map = pickle.load(f)
 
     obs, _ = env.reset(map=map)
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     action_counter = 0
     
     # Load Horizon Prediction Model
-    model = PPO.load("../../train_data/horizon_only/ppo_mpc_horizon_ks_1-3d_complex")
+    model = PPO.load("../../train_data/horizon_only/ppo_mpc_horizon_ks_1-3d_gates")
 
     # print(f"Initial observation: {obs[:4]}... (truncated)")
     log = []
